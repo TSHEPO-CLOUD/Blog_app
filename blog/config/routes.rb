d3fedd683744e79643be0a7da0f1d880e6e23dba
nna nna 19 <uchennaongit@gmail.com>
2021-01-11 13:03:05 +0100

    update the welcome page

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: 'blog_apps#index'
+  root to: 'blog_apps#welcome'
   resources :blog_apps
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
